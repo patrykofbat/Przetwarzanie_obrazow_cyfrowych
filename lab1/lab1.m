f98de4bf0c90c1cbfc41ad7e8b848354125b384e
lsriw <lsriw@debian>
2018-10-02 18:04:58 +0200

    init commit

---
+++ lab1/lab1.m
@@ -0,0 +1,14 @@
+info_jpg = imfinfo('lena.jpg')
+info_bmp = imfinfo('lena.bmp')
+
+img_jpg = imread('lena.jpg');
+img_bmp = imread('lena.bmp');
+imshow(img_jpg);
+figure;
+imshow(img_bmp);
+figure;
+
+img_bmp_gray = rgb2gray(img_bmp);
+imshow(img_bmp_gray);
+
+imwrite(img_bmp_gray, 'lena_gray.bmp');
